1133134d8b06714704977c8e62dabfd1c1434d82
mascarpona <iamirenbaksheeva@gmail.com>
2016-12-14 23:55:11 +0300

    Windows form

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,45 +28,264 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.stop = new System.Windows.Forms.Button();
+            this.START = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Place = new System.Windows.Forms.Button();
+            this.random = new System.Windows.Forms.Button();
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.turn = new System.Windows.Forms.Label();
+            this.Enemy = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Red;
+            this.stop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.stop.Font = new System.Drawing.Font("Lucida Bright", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.stop.Location = new System.Drawing.Point(82, 57);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(76, 73);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "STOP";
+            this.stop.UseVisualStyleBackColor = false;
+            // 
+            // START
+            // 
+            this.START.BackColor = System.Drawing.Color.GreenYellow;
+            this.START.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.START.Font = new System.Drawing.Font("Lucida Bright", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.START.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.START.Location = new System.Drawing.Point(82, 332);
+            this.START.Name = "START";
+            this.START.Size = new System.Drawing.Size(77, 53);
+            this.START.TabIndex = 6;
+            this.START.Text = "START";
+            this.START.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(97, 106);
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.GreenYellow;
+            this.button1.Location = new System.Drawing.Point(72, 391);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Size = new System.Drawing.Size(99, 34);
+            this.button1.TabIndex = 37;
+            this.button1.Text = "Clean";
+            this.button1.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // Place
             // 
-            this.button2.Location = new System.Drawing.Point(134, 44);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Place.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.Place.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Place.Font = new System.Drawing.Font("Lucida Bright", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Place.Location = new System.Drawing.Point(29, 496);
+            this.Place.Name = "Place";
+            this.Place.Size = new System.Drawing.Size(83, 82);
+            this.Place.TabIndex = 38;
+            this.Place.Text = "Place";
+            this.Place.UseVisualStyleBackColor = false;
+            // 
+            // random
+            // 
+            this.random.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.random.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.random.Font = new System.Drawing.Font("Lucida Bright", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.random.Location = new System.Drawing.Point(118, 496);
+            this.random.Name = "random";
+            this.random.Size = new System.Drawing.Size(106, 82);
+            this.random.TabIndex = 39;
+            this.random.Text = "Random";
+            this.random.UseVisualStyleBackColor = false;
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Location = new System.Drawing.Point(659, 468);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(128, 21);
+            this.ComboBox.TabIndex = 40;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Enabled = false;
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(661, 496);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 20);
+            this.label4.TabIndex = 41;
+            this.label4.Tag = "4";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Enabled = false;
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(661, 543);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 42;
+            this.label3.Tag = "3";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Enabled = false;
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(799, 503);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.TabIndex = 43;
+            this.label5.Tag = "2";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.Enabled = false;
+            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+            this.label6.Location = new System.Drawing.Point(803, 543);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 20);
+            this.label6.TabIndex = 44;
+            this.label6.Tag = "1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.checkBox1.Font = new System.Drawing.Font("Lucida Bright", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.checkBox1.Location = new System.Drawing.Point(641, 159);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(160, 24);
+            this.checkBox1.TabIndex = 45;
+            this.checkBox1.Text = "Zone of Silense";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(533, 440);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 46;
+            this.label7.Text = "Killed : 0";
+            // 
+            // turn
+            // 
+            this.turn.AutoSize = true;
+            this.turn.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.turn.Font = new System.Drawing.Font("Lucida Bright", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.turn.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.turn.Location = new System.Drawing.Point(477, 9);
+            this.turn.Name = "turn";
+            this.turn.Size = new System.Drawing.Size(103, 20);
+            this.turn.TabIndex = 47;
+            this.turn.Text = "YOU TURN";
+            // 
+            // Enemy
+            // 
+            this.Enemy.AutoSize = true;
+            this.Enemy.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Enemy.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Enemy.ForeColor = System.Drawing.Color.Maroon;
+            this.Enemy.Location = new System.Drawing.Point(375, 9);
+            this.Enemy.Name = "Enemy";
+            this.Enemy.Size = new System.Drawing.Size(63, 17);
+            this.Enemy.TabIndex = 48;
+            this.Enemy.Text = "ENEMY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(393, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "YOU";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(533, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Killed : 0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button2);
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.Control0;
+            this.ClientSize = new System.Drawing.Size(851, 624);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Enemy);
+            this.Controls.Add(this.turn);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ComboBox);
+            this.Controls.Add(this.random);
+            this.Controls.Add(this.Place);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.START);
+            this.Controls.Add(this.stop);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SeaBattle";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button START;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Place;
+        private System.Windows.Forms.Button random;
+        private System.Windows.Forms.ComboBox ComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label turn;
+        private System.Windows.Forms.Label Enemy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+
     }
 }
 
