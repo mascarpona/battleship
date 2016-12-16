d695416afd001981b0804b58696e519ad42c0924
1 <1@1->
2016-12-17 00:02:05 +0400

    pictures fixed

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -23,37 +23,37 @@ namespace WindowsFormsApplication1
             {
                 label1.Size = new Size(20, 80);
                 label1.Location = new Point(comboBox1.Location.X + 1, 495);
-                label1.Image = Image.FromFile(Application.StartupPath + @"\" + "4_.png");
+                label1.Image = Image.FromFile(Application.StartupPath + @"\..\..\Resources\" + "4_.png");
 
                 label2.Size = new Size(20, 60);
                 label2.Location = new Point(comboBox1.Location.X + 70, 495);
-                label2.Image = Image.FromFile(Application.StartupPath + @"\" + "3_.png");
+                label2.Image = Image.FromFile(Application.StartupPath + @"\..\..\Resources\" + "3_.png");
 
                 label3.Size = new Size(20, 40);
                 label3.Location = new Point(comboBox1.Location.X + 142, 495);
-                label3.Image = Image.FromFile(Application.StartupPath + @"\" + "2_.png");
+                label3.Image = Image.FromFile(Application.StartupPath + @"\..\..\Resources\" + "2_.png");
 
                 label4.Size = new Size(20, 20);
                 label4.Location = new Point(comboBox1.Location.X + 142, 545);
-                label4.Image = Image.FromFile(Application.StartupPath + @"\" + "1_.png");
+                label4.Image = Image.FromFile(Application.StartupPath + @"\..\..\Resources\" + "1_.png");
             }
             else
             {
                 label1.Size = new Size(80, 20);
                 label1.Location = new Point(comboBox1.Location.X + 1, 496);
-                label1.Image = Image.FromFile(Application.StartupPath + @"\" + "4.png");
+                label1.Image = Image.FromFile(Application.StartupPath + @"\..\..\Resources\" + "4.png");
 
                 label2.Size = new Size(60, 20);
                 label2.Location = new Point(comboBox1.Location.X + 1, 543);
-                label2.Image = Image.FromFile(Application.StartupPath + @"\" + "3.png");
+                label2.Image = Image.FromFile(Application.StartupPath + @"\..\..\Resources\" + "3.png");
 
                 label3.Size = new Size(40, 20);
                 label3.Location = new Point(comboBox1.Location.X + 140, 503);
-                label3.Image = Image.FromFile(Application.StartupPath + @"\" + "2.png");
+                label3.Image = Image.FromFile(Application.StartupPath + @"\..\..\Resources\" + "2.png");
 
                 label4.Size = new Size(20, 20);
                 label4.Location = new Point(comboBox1.Location.X + 143, 543);
-                label4.Image = Image.FromFile(Application.StartupPath + @"\" + "1.png");
+                label4.Image = Image.FromFile(Application.StartupPath + @"\..\..\Resources\" + "1.png");
             }
         }
 
